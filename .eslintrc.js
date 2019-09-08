31cac69eabd5438e58be31818241992b5c04dd02
Andrew K <rew.kha@gmail.com>
2019-09-08 15:57:28 +0200

    Initilize the express with webpack

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 2019,
+    sourceType: 'module'
+  },
+  env: {
+    es6: true,
+    node: true,
+    browser: true
+  },
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['**/*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  extends: 'eslint:recommended',
+  rules: {
+    indent: ['error', 2, { 'SwitchCase': 1 }],
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 0,
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
+  }
+};
